502e9be5dbd6d772421e1a2df5e874c071ecd930
root <kangrotnengak@gmail.com>
2024-02-06 06:39:27 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    list_argv = sys.argv
-    list_argv.pop(0)
-
-    liat_len = len(list_argv)
-    arg = ""
-
-    if liat_len == 0:
-        arg = "arguments" + "."
-    elif liat_len == 1:
-        arg = "argument" + ":"
+    arg = sys.argv
+    arg.pop(0)
+    lenght = len(arg)
+    if lenght == 0:
+        print("0 arguments.")
+    elif lenght == 1:
+        print(f"1 argument:\n1: {arg[0]}")
     else:
-        arg = "arguments" + ":"
-    print(f"{liat_len} {arg}")
-
-    length = liat_len - 1
-
-    for num in range(liat_len):
-        print(f"{num + 1}: {list_argv[num]}")
+        print(f"{lenght} arguments:")
+        for num, item in enumerate(arg):
+            print(f"{num + 1}: {item}")
